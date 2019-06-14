e18e553f4aee478a40ee7e367349042c5072a885
Carlos Sandoval <iznomag@gmail.com>
2019-06-14 08:54:06 -0500

    Create Main Component (#13)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Main from "./views/Main";
+import { Global } from "@emotion/core";
+
+function App() {
+  const [username, setUsername] = React.useState(
+    localStorage.getItem("username")
+  );
+
+  return (
+    <>
+      <Global
+        styles={{
+          body: {
+            margin: 0,
+            padding: 0
+          }
+        }}
+      />
+      <Main />
+    </>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
